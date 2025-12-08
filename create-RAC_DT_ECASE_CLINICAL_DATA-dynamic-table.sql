67c02005b4a507970707ae6dcc4b3570d4e1f392
Neil Siregar <neil.siregar@gmail.com>
2025-12-08 17:29:31 +1100

    Updated the create-RAC-DT-ECASE_CLINICAL_DATA

--- create-RAC_DT_ECASE_CLINICAL_DATA-dynamic-table.sql
+++ create-RAC_DT_ECASE_CLINICAL_DATA-dynamic-table.sql
@@ -3,34 +3,50 @@ USE WAREHOUSE DEV_WH;
 USE DATABASE HH_DEV;
 USE SCHEMA BRONZE_DB;
 
-SELECT * FROM GOLD_DB.DIM_RAC_DT_ECASE_CLINICAL_DATA;
+-- drop table GOLD_DB.DIM_RAC_DT_ECASE_CLINICAL_DATA;
+-- select * from GOLD_DB.DIM_RAC_DT_ECASE_CLINICAL_DATA;
 
 CREATE OR REPLACE DYNAMIC TABLE GOLD_DB.DIM_RAC_DT_ECASE_CLINICAL_DATA
-TARGET_LAG = '5 minutes'
-WAREHOUSE = DEV_WH
+    TARGET_LAG = '5 minutes'
+    WAREHOUSE = DEV_WH
 AS
 WITH SiteStartDates AS (
     SELECT 'Ingle Farm' AS Site, TO_DATE('2025-11-03') AS StartDate
-    UNION ALL
+    UNION ALL 
     SELECT 'Port Pirie', TO_DATE('2025-11-17')
 ),
 
 distinct_incclass AS (
     SELECT 'Infection' AS IncClassPrimary
-    UNION ALL SELECT 'SIRS \\ ' || LongDesc FROM BRONZE_DB.ECASE_ASSAULT_TYPES_RAW
-    UNION ALL SELECT 'Wounds \\ ' || LongDesc FROM BRONZE_DB.ECASE_WOUND_TYPES_RAW
-    UNION ALL SELECT 'Incidents \\ ' || LongDesc FROM BRONZE_DB.ECASE_INCIDENT_TYPES_RAW
-    UNION ALL SELECT 'MedIncidents \\ ' || LongDesc FROM BRONZE_DB.ECASE_MEDINCIDENT_TYPE_RAW
+    UNION ALL
+    SELECT 'SIRS \\ ' || LongDesc FROM BRONZE_DB.ECASE_ASSAULT_TYPES_RAW
+    UNION ALL
+    SELECT 'Wounds \\ ' || LongDesc FROM BRONZE_DB.ECASE_WOUND_TYPES_RAW
+    UNION ALL
+    SELECT 'Incidents \\ ' || LongDesc FROM BRONZE_DB.ECASE_INCIDENT_TYPES_RAW
+    UNION ALL
+    SELECT 'MedIncidents \\ ' || LongDesc FROM BRONZE_DB.ECASE_MEDINCIDENT_TYPE_RAW
 ),
 
-/* Snowflake replacement for recursive calendar */
+/* Recursive calendar per site */
 Calendar AS (
-    SELECT 
-        s.Site,
-        DATEADD('day', seq4(), s.StartDate) AS IncidentDate
-    FROM SiteStartDates s
-    JOIN TABLE(GENERATOR(ROWCOUNT => 2000)) g
-    WHERE DATEADD('day', seq4(), s.StartDate) <= CURRENT_DATE()
+    WITH RECURSIVE c AS (
+        SELECT 
+            Site,
+            StartDate AS IncidentDate
+        FROM SiteStartDates
+
+        UNION ALL
+
+        SELECT 
+            c.Site,
+            DATEADD(day, 1, c.IncidentDate)
+        FROM c
+        JOIN SiteStartDates s 
+            ON c.Site = s.Site
+        WHERE DATEADD(day, 1, c.IncidentDate) <= CURRENT_DATE()
+    )
+    SELECT * FROM c
 ),
 
 RiskStrat AS (
@@ -42,18 +58,32 @@ RiskStrat AS (
 ),
 
 AllDateInclass AS (
-    SELECT c.IncidentDate, d.IncClassPrimary, c.Site, r.RiskStrat
+    SELECT 
+        c.IncidentDate,
+        d.IncClassPrimary,
+        c.Site,
+        r.RiskStrat
     FROM Calendar c
     CROSS JOIN distinct_incclass d
     CROSS JOIN RiskStrat r
 ),
 
+/* eCase clinical data (from your conformed dynamic table / view) */
 DATA1 AS (
-    SELECT
-        Status, BaseID, Program, IncidentDate, IncidentTime,
-        IncClassPrimary, InfType,
-        CASE WHEN Site = 'Lealholme (Port Pirie)' THEN 'Port Pirie' ELSE Site END AS Site,
-        Incident_Location, IncidentCount,
+    SELECT 
+        Status,
+        BaseID,
+        Program,
+        IncidentDate,
+        IncidentTime,
+        IncClassPrimary,
+        InfType,
+        CASE 
+            WHEN Site = 'Lealholme (Port Pirie)' THEN 'Port Pirie'
+            ELSE Site
+        END AS Site,
+        Incident_Location,
+        IncidentCount,
         CASE 
             WHEN RiskStrat = '1' THEN 'SAC 1 - Extreme'
             WHEN RiskStrat = '2' THEN 'SAC 2 - High'
@@ -61,53 +91,64 @@ DATA1 AS (
             WHEN RiskStrat = '4' THEN 'SAC 4 - Low'
             ELSE 'Undefined'
         END AS RiskStrat,
-        SIRSIncType, SIRSVicPerp, FirstName, Surname, ResidentID,
-        Description, Detail, SIRSIncCat, SIRSDegreeHarm,
-        ResidentType, ResidentStatus
+        SIRSIncType,
+        SIRSVicPerp,
+        Firstname,
+        Surname,
+        ResidentID,
+        Description,
+        Detail,
+        SIRSIncCat,
+        SIRSDegreeHarm,
+        ResidentType,
+        residentStatus
     FROM SILVER_DB.ECASE_RAC_DT_POWERBI_CLINICAL_CONFORMED
 ),
 
+/* Procura / CUSTTABLE mapping */
 procura_data AS (
     SELECT 
         AccountNum,
         CASE 
-            /* TRY_TO_NUMBER() = Snowflake safe version of ISNUMERIC() */
             WHEN TRY_TO_NUMBER(HHAC_ECASERESIDENTID) IS NULL THEN '999999'
             ELSE HHAC_ECASERESIDENTID
         END AS HHAC_ECASERESIDENTID
-    FROM GOLD_DB.DIM_CUSTTABLE
+    FROM GOLD_DB.DIM_CUSTTABLE      -- <== change to your actual Procura CUSTTABLE table
     WHERE HHAC_ECASERESIDENTID <> ''
 )
 
-SELECT 
-    c.Status,
-    c.BaseID,
-    'Residential' AS Program,
-    a.IncidentDate,
-    c.IncidentTime,
-    a.IncClassPrimary,
-    c.InfType,
-    a.Site,
-    c.Incident_Location,
-    COALESCE(c.IncidentCount, 0) AS IncidentCount,
-    a.RiskStrat,
-    c.SIRSIncType,
-    c.SIRSVicPerp,
-    c.FirstName,
-    c.Surname,
-    ct.AccountNum AS MedicalRecordNo,
-    c.ResidentID AS eCase_ResidentID,
-    c.Description,
-    c.Detail,
-    c.SIRSIncCat,
-    c.SIRSDegreeHarm,
-    CURRENT_TIMESTAMP() AS LOADED_AT
+/* Final output – quoted aliases preserve exact case */
+SELECT
+    c.Status            AS "Status",
+    c.BaseID            AS "BaseID",
+    'Residential'       AS "Program",
+    a.IncidentDate      AS "IncidentDate",
+    c.IncidentTime      AS "IncidentTime",
+    a.IncClassPrimary   AS "IncClassPrimary",
+    c.InfType           AS "InfType",
+    a.Site              AS "Site",
+    c.Incident_Location AS "Incident_Location",
+    COALESCE(c.IncidentCount, 0) AS "IncidentCount",
+    a.RiskStrat         AS "RiskStrat",
+    c.SIRSIncType       AS "SIRSIncType",
+    c.SIRSVicPerp       AS "SIRSVicPerp",
+    c.Firstname         AS "Firstname",
+    c.Surname           AS "Surname",
+    ct.AccountNum       AS "MedicalRecordNo",
+    c.ResidentID        AS "eCase_ResidentID",
+    c.Description       AS "Description",
+    c.Detail            AS "Detail",
+    c.SIRSIncCat        AS "SIRSIncCat",
+    c.SIRSDegreeHarm    AS "SIRSDEgreeHarm",  -- keeping your original final name
+    CURRENT_TIMESTAMP() AS "LOADED_AT"
 FROM AllDateInclass a
 LEFT JOIN DATA1 c
-    ON c.IncidentDate = a.IncidentDate
+    ON c.IncidentDate    = a.IncidentDate
    AND c.IncClassPrimary = a.IncClassPrimary
-   AND c.Site = a.Site
-   AND c.RiskStrat = a.RiskStrat
+   AND c.Site            = a.Site
+   AND c.RiskStrat       = a.RiskStrat
 LEFT JOIN procura_data ct
     ON c.ResidentID = ct.HHAC_ECASERESIDENTID;
+
+
 
